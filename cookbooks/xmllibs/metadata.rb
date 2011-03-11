60a115de4f757cdcdb62b6f032cdc254c63a5c76
Ross Cooperman <cooperman@gmail.com>
2011-03-11 14:56:47 -0500

    Added a recipe for installing XML libraries.

---
+++ cookbooks/xmllibs/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Ross Cooperman"
+maintainer_email "cooperman@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures xmllibs"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
