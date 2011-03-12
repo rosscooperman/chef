3db0af470b2d851560567314ddaa2e123a41d878
Ross Cooperman <cooperman@gmail.com>
2011-03-12 00:56:20 -0500

    Removed references to now missing README files.

--- cookbooks/cap-deploy-user/metadata.rb
+++ cookbooks/cap-deploy-user/metadata.rb
@@ -2,5 +2,4 @@ maintainer       "Ross Cooperman"
 maintainer_email "cooperman@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures cap-deploy-user"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
